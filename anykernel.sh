b55a2386250072889d144512c0a317b0438d66f7
0ctobot <return.of.octobot@gmail.com>
2019-06-04 00:27:45 -0400

    neutrino: anykernel.sh: Support both OOS and custom ROMs in the same zip
    We can build both images and include them in the zip then dynamically
    move them to where they need to go.
    
    [@0ctobot: Adapted from nathanchance/AnyKernel2-archive@bfa82cf]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -33,6 +33,20 @@ ramdisk_compression=gzip
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+## Select the correct image to flash
+userflavor="$(file_getprop /system/build.prop "ro.build.user"):$(file_getprop /system/build.prop "ro.build.flavor")";
+case "$userflavor" in
+  jenkins:qssi-user) os="stock"; os_string="OxygenOS/HydrogenOS";;
+  *) os="custom"; os_string="a custom ROM";;
+esac;
+ui_print " " "You are on $os_string!";
+if [ -f $home/kernels/$os/Image.gz ] && [ -f $home/kernels/$os/dtb ]; then
+  mv $home/kernels/$os/Image.gz $home/Image.gz;
+  mv $home/kernels/$os/dtb $home/dtb;
+else
+  ui_print " " "There is no kernel for your OS in this zip! Aborting..."; exit 1;
+fi;
+
 ## AnyKernel install
 dump_boot;
 
