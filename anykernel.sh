da8870991de3f7ce9c3fdac0a4c10aeafbe508f0
0ctobot <return.of.octobot@gmail.com>
2019-06-04 00:27:45 -0400

    neutrino: anykernel.sh: Support both OOS and custom ROMs in the same zip
    We can build both images and include them in the zip then dynamically
    move them to where they need to go.
    
    [@0ctobot: Adapted from nathanchance/AnyKernel2-archive@bfa82cf]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -13,6 +13,17 @@ eval $(cat $home/props | grep -v '\.');
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+## Select the correct image to flash
+userflavor="$(file_getprop /system/build.prop "ro.build.user"):$(file_getprop /system/build.prop "ro.build.flavor")";
+case "$userflavor" in
+  OnePlus:OnePlus6-user|OnePlus:OnePlus6T-user) os="stock"; os_string="OxygenOS";;
+  *) os="custom"; os_string="a custom ROM";;
+esac;
+ui_print " " "You are on $os_string!";
+if [ ! -f $home/kernels/$os/Image.gz ]; then
+  ui_print " " "There is no kernel for your OS in this zip! Aborting..."; exit 1;
+fi;
+
 ## AnyKernel install
 dump_boot;
 write_boot;
