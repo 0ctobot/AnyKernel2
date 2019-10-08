fe67fb8cc738e3220bebaba7913137ab390e6852
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-08 23:41:33 +0900

    Load properties externally
    Load a separate "props" file for better modularity
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [@0ctobot: Squash arter97/AnyKernel3@39b8346 with arter97/AnyKernel3@248d8ed]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,34 +1,16 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
-properties() { '
-kernel.string=Neutrino Kernel for the OnePlus 7 Series by @0ctobot
-do.devicecheck=1
-do.modules=0
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=OnePlus7
-device.name2=guacamoleb
-device.name3=OnePlus7Pro
-device.name4=guacamole
-device.name5=OnePlus7ProTMO
-device.name6=guacamolet
-device.name7=OnePlus7ProNR
-device.name8=OnePlus7T
-device.name9=hotdogb
-device.name10=OnePlus7TPro
-device.name11=hotdog
-device.name12=OnePlus7TProNR
-device.name13=hotdogg
-supported.versions=10 - 11
-'; } # end properties
+# set up working directory variables
+test "$home" || home=$PWD;
+bootimg=$home/boot.img;
+bin=$home/tools;
+patch=$home/patch;
+ramdisk=$home/ramdisk;
+split_img=$home/split_img;
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot
-is_slot_device=1
-ramdisk_compression=gzip
+## AnyKernel setup
+eval $(cat $home/props | grep -v '\.')
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
